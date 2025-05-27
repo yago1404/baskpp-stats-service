ce867551f6a4052b1f264f368c1e617d0bf2a27f
Yago Taveiros Ferreira <ytf@ic.ufal.br>
2025-05-27 20:00:43 -0300

    fix: lint

--- models/median.py
+++ models/median.py
@@ -1,6 +1,7 @@
 class Median:
     def __init__(self, free_trow: float, failure_free_trow: float,
-                 two_points: float, failure_two_points: float, tree_points: float, failure_tree_points: float, assists: float,
+                 two_points: float, failure_two_points: float, tree_points: float, failure_tree_points: float,
+                 assists: float,
                  steals: float, rebounds: float, blocks: float, falts: float, turnovers: float):
         self.points = (free_trow + (two_points * 2) + (tree_points * 3)) / 3
         self.free_trow = free_trow
@@ -15,7 +16,6 @@ class Median:
         self.blocks = blocks
         self.falts = falts
         self.turnovers = turnovers
-
 
     @staticmethod
     def empty():
