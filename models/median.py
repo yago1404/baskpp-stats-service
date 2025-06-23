c695f08336f0a252133de32f08e6555d239819f3
Yago Taveiros Ferreira <ytf@ic.ufal.br>
2025-06-23 14:03:59 -0300

    feat: create add play method to model

--- models/median.py
+++ models/median.py
@@ -1,3 +1,6 @@
+from models.stat import Stat
+
+
 class Median:
     def __init__(self, points: float, free_trow: float, failure_free_trow: float,
                  two_points: float, failure_two_points: float, tree_points: float, failure_tree_points: float,
@@ -19,5 +22,33 @@ class Median:
         self.turnovers = turnovers
 
     @staticmethod
-    def empty():
-        return Median(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+    def empty(game_id=None):
+        return Median(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, game_id=game_id)
+
+    def add_play(self, stat: Stat):
+        if stat.free_trow is not None:
+            self.free_trow += stat.free_trow
+        if stat.failure_free_trow is not None:
+            self.failure_free_trow += stat.failure_free_trow
+        if stat.two_points is not None:
+            self.two_points += stat.two_points
+        if stat.failure_two_points is not None:
+            self.failure_two_points += stat.failure_two_points
+        if stat.tree_points is not None:
+            self.tree_points += stat.tree_points
+        if stat.failure_tree_points is not None:
+            self.failure_tree_points += stat.failure_tree_points
+        if stat.assists is not None:
+            self.assists += stat.assists
+        if stat.steals is not None:
+            self.steals += stat.steals
+        if stat.rebounds is not None:
+            self.rebounds += stat.rebounds
+        if stat.blocks is not None:
+            self.blocks += stat.blocks
+        if stat.falts is not None:
+            self.falts += stat.falts
+        if stat.turnovers is not None:
+            self.turnovers += stat.turnovers
+
+        self.points = self.free_trow + (self.two_points * 2) + (self.tree_points * 3)
